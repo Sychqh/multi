cb6580111fbc92d40bfba004f4928c1c95cce177
Sychqh <sychqh@gmail.com>
2021-12-02 19:48:04 +0500

    Асинхронный IO-bound, скрины в README.md (теперь реально)

--- IO-bound.py
+++ IO-bound.py
@@ -1,9 +1,12 @@
+import concurrent.futures
+import urllib.request
 from urllib.request import Request, urlopen
 from urllib.parse import unquote
 
 links = open('res.txt', encoding='utf8').read().split('\n')
 
-for url in links:
+
+def load_url(url):
     try:
         request = Request(
             url,
@@ -11,7 +14,19 @@ for url in links:
         )
         resp = urlopen(request, timeout=5)
         code = resp.code
-        print(code)
         resp.close()
+        return code
     except Exception as e:
         print(url, e)
+
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+    future_to_url = {executor.submit(load_url, url): url for url in links}
+    for future in concurrent.futures.as_completed(future_to_url):
+        url = future_to_url[future]
+        try:
+            data = future.result()
+        except Exception as exc:
+            print('%r generated an exception: %s' % (url, exc))
+        else:
+            print(data)
